d74c1de740175fa16e6d91ef36b6357abf327552
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-07 16:46:06 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _abs - return abs value
- *
+ * _abs - return absolute value of an integer
+ * @n: integer to get absolute value 
  * Return : 0 or 1
  */
 int _abs(int n)
