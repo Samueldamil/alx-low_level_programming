4eef18c6d76c04ec4584244e277377f003ed0a44
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-07 15:54:23 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,13 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _isalpha - check alphabetic character
- * @c: is c integer arguments
- * Return: 0
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
  */
+
 int _isalpha(int c)
 {
-	
-	if (c >= 97 && c <= 122);
-	{
-		return (1);
-	}
-	else if (c >= 65 && c <= 90);
-	{
-		return (1);
-	}
-	return (0);
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
 }
