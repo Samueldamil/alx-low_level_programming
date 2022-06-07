5772dd577f3ca622c54b6bc28e9ec4228be2c829
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-07 16:12:37 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		test = 1;
-		_putchar('1');
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
@@ -21,7 +21,7 @@ int print_sign(int n)
 	else
 	{
 		test = -1;
-		_putchar('-1');
+		_putchar('-');
 	}
 	return (test);
 }
