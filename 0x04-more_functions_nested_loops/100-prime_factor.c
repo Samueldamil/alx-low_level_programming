6eee54e6eb8b57ca542b6ea645a5417844fe042d
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-09 17:32:10 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,21 +5,23 @@
  *
  * Return: 0
  */
+
 int main(void)
 {
-	
+
 unsigned int long n = 612852475143, a = (int) sqrt(n);
-	
+
 while (1)
 {
+
 	if (n % a == 0)
 	{
 		printf("%lu \n", n / a);
 		break;
-	
 	}
 	a--;
+
 }
-	
+
 return (0);
 }
