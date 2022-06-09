33623d31d6c6ccb84cdd5df016a4d38cda15038b
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-09 14:05:06 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_lines - print straight line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
