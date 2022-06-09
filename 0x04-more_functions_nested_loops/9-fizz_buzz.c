96faac862cfd883b57827f01ee8ab17a1c9b5f4c
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-09 16:33:06 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * 
- * Return: void
+ * main - check the code for Holberton School students.
+ *
+ * Return: void.
  */
+
 int main(void)
 {
+
 	int i = 1;
-	
+
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -19,19 +21,22 @@ int main(void)
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
- 		{
+		{
 			printf("Buzz");
 		}
 		else
 		{
 			printf("%i", i);
 		}
+
 		if (i != 100)
 		{
 			putchar(' ');
 		}
-		i++
+
+		i++;
 	}
 	putchar('\n');
 	return (0);
+
 }
