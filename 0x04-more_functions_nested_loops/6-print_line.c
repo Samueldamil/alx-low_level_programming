cb7b42e15c04b6612a4e1bec5036b86a80a1e7a1
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-09 14:12:18 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_lines - print straight line
- * Return: void
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
 
 void print_line(int n)
 {
-	
+
 	int i = 0;
 
 	while (i < n && n > 0)
