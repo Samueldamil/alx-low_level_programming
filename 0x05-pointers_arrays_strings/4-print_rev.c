6ba5919c684c96a956d00e1b9291f526fc877ad9
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-13 18:36:59 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s : pointer to string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while(s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n')
+}
