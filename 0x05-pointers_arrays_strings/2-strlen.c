d4b005cc076e9ed22e34de819d5fd4009a50ae77
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-13 18:04:03 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - find the length of the string
+ * @s - pointer to string
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (s[i]);
+		i++;
+	return (0);
+}
