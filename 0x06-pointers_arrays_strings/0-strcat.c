9058ddc4d81e145ed5a2e63a6a877c5d1cf83d96
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-15 21:38:51 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int d, s;
+	int d = 0;
+	int s = 0;
 
-	for (d = 0; dest[d] != '\0'; d++)
+	for (dest[d] != '\0'; d++)
 	;
-	for (s = 0; src[s] != '\0'; s++)
+	for (src[s] != '\0'; s++)
 	{
 		d++;
 		dest[d] = src[s];
