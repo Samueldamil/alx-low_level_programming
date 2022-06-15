2f1e8661678cce0a55a119430af809da724b285c
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-15 23:53:30 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[dest_len + i] = src[i];
-/*should end with null*/
-		dest[dest_len] = '\0';
-
-	return (0);
+	}
+	
+	dest[dest_len] = '\0';
+	return (dest);
 }
