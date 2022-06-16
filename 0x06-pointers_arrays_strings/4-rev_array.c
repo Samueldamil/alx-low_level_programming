fa5e405052e95b42a3845e14bddf85cae280c025
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-16 07:28:22 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+ 
+/**
+ * reverse_array - reverse the content of the array
+ * @n: number of element in the array
+ */
+void reverse_array(int *a, int n)
+}
+	int b;
+	int c;
+	int temp;
+
+	b = 0;
+	c = n - 1
+	
+	while (b < c)
+	{
+		temp = a[b];
+		a[b] = a[c];
+		a[c] = temp;
+		i++;
+		j--;
+	}
+}
