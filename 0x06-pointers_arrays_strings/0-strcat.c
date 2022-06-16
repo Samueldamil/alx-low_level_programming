dbdc2796e0a1b8cbd3a3a192431364ffeb4d11c2
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-16 04:04:14 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,24 +6,26 @@
  * @src: pointer source
  * Return pointer to dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int d = 0;
-	int s = 0;
+	int i;
+	int j;
 
-	while (dest[d] != '\0')
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		d++;
+		i++;
 	}
 
-	while (src[s] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[d] = src[s];
-		s++;
-		d++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
 
-	dest[d] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
