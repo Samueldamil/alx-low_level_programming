d67ffa8056f6919bb2471a229a55348a0b5b1b8a
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-16 15:00:06 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase letters to uppercase
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+	return (str);
+}
