1e866bfb5922acc50813a8d494d6737181f9035c
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-20 22:11:16 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
- * *_strpbrk - searches for a string
- * @s : string
- * @accept : string to match
- * Return : 's' or NULL if no byte os found
-*/
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 	char *p;
 
 	i = 0;
-	while (i != '\0')
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (j != '\0')
+		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
 			{
@@ -25,5 +28,6 @@ char *_strpbrk(char *s, char *accept)
 		}
 		i++;
 	}
+
 	return (0);
 }
