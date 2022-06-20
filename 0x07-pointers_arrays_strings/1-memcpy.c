f3e10466556d8627637c74e92bf07ba5b037fb50
Samueldamil <damilareedun@gmail.com>
2022-06-20 10:07:23 -0700

    pointer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,16 @@
 /**
  * _memcpy - copy memory area from source to destination
- * @src : source file
- * @dest : destination file
- * Return : destination
+ * @src: source file
+ * @dest: destination file
+ * @n : memory area
+ * Return: destination
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
 
-	for (index = 0; index < n; index++) 
+	for (index = 0; index < n; index++)
 		*(dest + index) = *(src + index);
 	return (dest);
 }
