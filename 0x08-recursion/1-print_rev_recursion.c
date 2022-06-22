2d486da143b003b66b0276a81f460a56e28a8012
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-22 10:08:06 +0100

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s - pointer to string
+ * Return : void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s--;
+	}
+	else
+	{
+		_putchar('\n');
+		_print_rev_recursion(s - 1);
+	}
+}
