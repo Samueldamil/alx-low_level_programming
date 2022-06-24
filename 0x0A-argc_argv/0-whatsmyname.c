4b689a33a3b676a14b08ada9300ed79eb773e429
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-06-24 15:21:40 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: argument counts
+ * @argv: argument vendor
+ * 
+ * Return: 0
+ */
+int main(int _attribute((_unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
