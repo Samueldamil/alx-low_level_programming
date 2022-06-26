d82fb4893e7f8dfe779ced04c039692cb7feba67
Samueldamil <damilareedun@gmail.com>
2022-06-26 19:09:03 +0100

    memset

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: byte fill count
+ * Return: memory space
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
