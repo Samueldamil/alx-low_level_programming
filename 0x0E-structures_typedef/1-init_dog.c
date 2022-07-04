2d645d5726d4aecd5c50e8d023e4abca1afb5b26
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-07-04 20:49:58 +0100

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initializes variable of type strict dog
+ * @d : struct dog
+ * @name : string
+ * @age : integer
+ * @owner : string
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
