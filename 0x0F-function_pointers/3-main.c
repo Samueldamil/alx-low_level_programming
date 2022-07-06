16c3d60d8d608caf7a2900c57e1b983784a064e5
Samueldamil <damilareedun@gmail.com>
2022-07-06 16:26:25 +0100

    file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,28 +8,18 @@
 *
 * Return: Always 0.
 */
-int main(int arc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2, (*operate)(int, int);
+	int R;
 
-	if (arc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	if ((*arg[2] != '+' && *arg[2] != '-' && *arg[2] != '*' && *arg[2] != '/' && *arg[2] != '%') || *arg[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit (99);
-	}
-	num1 = atoi(arg[1]);
-	num2 = atoi(arg[3]);
-	if ((*arg[2] ==  '/' || *arg[2] == '%') && (num2 == 0))
-	{
-		printf("Error\n");
-		exit (100);
-	}
-	operate =  get_op_func(arg[2]);
-	printf("%d\n", operate(num1, num2));
+
+	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", R);
+
 	return (0);
 }
