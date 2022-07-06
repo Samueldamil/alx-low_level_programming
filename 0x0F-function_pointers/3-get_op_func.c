f4cf5ebe035a5979fffa8b576a428baeaa74e3c8
Samueldamil <damilareedun@gmail.com>
2022-07-06 14:39:00 +0100

    file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * *get_op_func - function that select the correct functio
+ * n to perform operation asked by the user
+ * @s: operator argument
+ * Return: function pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add two integers
+ * @a : integer
+ * @b : integer
+ * Return: sum of the two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract b from a
+ * @a : integer
+ * @b : integer
+ * Return: differences between two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: integer
+ * @b: integer
+ * Return: answer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a by b
+ * @a: integer
+ * @b: integer
+ * Return: answer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - find the remainder
+ * @a : integer
+ * @b : integer
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
