d9fbd81fee51cfc156dc94e58081e0e70eb0093b
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-07-06 16:42:43 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,13 +1,14 @@
-#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
+
 /**
-* main - Prints the result of simple operations.
-* @argc: The number of arguments supplied to the program.
-* @argv: An array of pointers to the arguments.
-*
-* Return: Always 0.
-*/
+ * main - main file
+ * @argc: number of lines arguments
+ * @argv: array of elements
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int R;
@@ -16,6 +17,13 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(98);
+	}
+
+	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", R);
+
+	return (0);
+}		exit(98);
 	}
 
 	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
