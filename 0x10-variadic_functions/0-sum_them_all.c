daca3c01d8eb94e8405dd71118fa751661dac1a7
Samueldamil <104783530+Samueldamil@users.noreply.github.com>
2022-07-07 16:16:59 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list plus;
-	unsigned int i, sum = 0;
+va_list list;
+unsigned int i, sum = 0;
 
-	va_start(plus, n);
-	if (n != 0)
-	for (i = 0; i < n; sum += va_arg(plus, unsigned int),i++)
-		;
-	va_end(plus);
+va_start(list, n);
+if (n != 0)
+	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+	;
+va_end(list);
 
-	return (sum);
+return (sum);
 }
